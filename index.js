732370d6ecf6a2ef077df8456eae033a6db62778
Lemein <lemeinannette@gmail.com>
2023-09-20 15:43:21 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper"
+let  welcomeMessage = "Welcome to Flatbook, ${Grace Hopper} !"
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase()
+let  shortGreeting = "Welcome, ${Grace} !"
+
+
+
